89f510cbd97efe89e352331ecbbdd878dae053ec
Amitpotdukhe <69644033+Amitpotdukhe@users.noreply.github.com>
2023-12-05 14:10:52 +0530

    cors

--- server/src/server.js
+++ server/src/server.js
@@ -14,16 +14,6 @@ app.use(cors(
         credentials: true,
     }))
 
-app.use(function (req, res, next) {
-    res.header('Content-Type', 'application/json;charset=UTF-8')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
-    res.header(
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    next()
-})
-
 const startDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI)
