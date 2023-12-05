dbccfbd4d26cc6a2fa8fb6feaf714066d88a82d1
Amitpotdukhe <69644033+Amitpotdukhe@users.noreply.github.com>
2023-12-05 13:56:32 +0530

    s

--- server/src/server.js
+++ server/src/server.js
@@ -17,6 +17,7 @@ app.use(cors(
 app.use(function (req, res, next) {
     res.header('Content-Type', 'application/json;charset=UTF-8')
     res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
     res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept'
